c0be27775f1ecf36f1288bc627e5128190388c9d
morenoa <Amoreno3657@Adams-MacBook-Pro.local>
2017-02-19 17:30:42 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/city_guid/Pier.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.city_guid;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Pier extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pier);
+    }
+
+}
---
+++ app/src/main/java/net/androidbootcamp/city_guid/Water.java
@@ -0,0 +1,14 @@
+package net.androidbootcamp.city_guid;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Water extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water);
+    }
+
+}
